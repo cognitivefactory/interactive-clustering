ae794eb0c84a458f854286df3e543b2b32e05ad9
Erwan Schild <erwan.schild@e-i.com>
2021-10-12 13:24:29 +0200

    refactor: update template with copier update

--- scripts/multirun.sh
+++ scripts/multirun.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-#PYTHON_VERSIONS="${PYTHON_VERSIONS-3.6 3.7 3.8 3.9}"
+# un/comment this if you want to enable/disable multiple Python versions
+PYTHON_VERSIONS="${PYTHON_VERSIONS-3.6 3.7 3.8 3.9 3.10 3.11}"
 
 if [ -n "${PYTHON_VERSIONS}" ]; then
     for python_version in ${PYTHON_VERSIONS}; do
-        if pdm use -f "${python_version}" &>/dev/null; then
+        if pdm use -f "${python_version}" &>/dev/null; then  # python${python_version}
             echo "> pdm run $@ (Python ${python_version})"
             pdm run "$@"
         else
-            echo "> pdm use -f ${python_version}: Python version not available?" >&2
+            echo "> pdm use -f ${python_version}: Python interpreter not available?" >&2  # python${python_version}
         fi
     done
 else
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-#PYTHON_VERSIONS="${PYTHON_VERSIONS-3.6 3.7 3.8 3.9}"
+# un/comment this if you want to enable/disable multiple Python versions
+PYTHON_VERSIONS="${PYTHON_VERSIONS-3.6 3.7 3.8 3.9 3.10 3.11}"
 
 install_with_pipx() {
     if ! command -v "$1" &>/dev/null; then
@@ -16,11 +17,11 @@ install_with_pipx pdm
 
 if [ -n "${PYTHON_VERSIONS}" ]; then
     for python_version in ${PYTHON_VERSIONS}; do
-        if pdm use -f "${python_version}" &>/dev/null; then
-            echo "> Using Python ${python_version} environment"
+        if pdm use -f "${python_version}" &>/dev/null; then  # python${python_version}
+            echo "> Using Python ${python_version} interpreter"
             pdm install
         else
-            echo "> pdm use -f ${python_version}: Python version not available?" >&2
+            echo "> pdm use -f ${python_version}: Python interpreter not available?" >&2  # python${python_version}
         fi
     done
 else
